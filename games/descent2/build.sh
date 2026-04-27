618eed161363ba376ac7b092ba0a32eb1c06ce86
Juan Lozano <juan@debian.localdomain>
2026-04-27 15:19:56 +0100

    added descent 2

--- games/descent2/build.sh
+++ games/descent2/build.sh
@@ -6,7 +6,7 @@ source "$CURRENT_DIR/../../utils/get-arch.sh"
 
 # Compose package name
 TARGET_ARCH=$(get_debian_arch)
-GAME="d1x-rebirth"
+GAME="d2x-rebirth"
 VERSION="0.60.0-beta2"
 
 sudo apt-get install -y scons libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libphysfs-dev libglu1-mesa-dev libgl1-mesa-dev || exit 1
@@ -20,13 +20,13 @@ sed -i 's/^static hud_x_scale_float HUD_SCALE_X/static constexpr hud_x_scale_flo
 sed -i 's/^static hud_y_scale_float HUD_SCALE_Y/static constexpr hud_y_scale_float HUD_SCALE_Y/' similar/main/gauges.cpp
 sed -i 's/^static hud_ar_scale_float HUD_SCALE_AR/static constexpr hud_ar_scale_float HUD_SCALE_AR/' similar/main/gauges.cpp
 # end fix
-scons sdl2=1 sdlmixer=1 d1x=1 -j$(nproc) || error "Failed to compile Descent 1 game engine!"
+scons sdl2=1 sdlmixer=1 d2x=1 -j$(nproc) || error "Failed to compile Descent 2 game engine!"
 
 # Compose the DEB package
 cd ..
 PACKAGE_NAME="${GAME}-${VERSION}_${TARGET_ARCH}"
 mkdir -p $PACKAGE_NAME/DEBIAN
-mkdir -p $PACKAGE_NAME/opt/d1x-rebirth
+mkdir -p $PACKAGE_NAME/opt/d2x-rebirth
 mkdir -p $PACKAGE_NAME/usr/bin
 mkdir -p $PACKAGE_NAME/usr/share/applications
 
@@ -34,37 +34,41 @@ mkdir -p $PACKAGE_NAME/usr/share/applications
 cp -R pkg/DEBIAN $PACKAGE_NAME/
 
 # Binary and icon
-cp dxx-rebirth/build/d1x-rebirth/d1x-rebirth $PACKAGE_NAME/opt/d1x-rebirth/d1x-rebirth
-cp pkg/icon.png $PACKAGE_NAME/opt/d1x-rebirth/icon.png
+cp dxx-rebirth/build/d2x-rebirth/d2x-rebirth $PACKAGE_NAME/opt/d2x-rebirth/
+cp pkg/icon.png $PACKAGE_NAME/opt/d2x-rebirth/icon.png
 
-# Shareware Assets
-wget https://web.archive.org/web/20221208193117if_/https://www.dxx-rebirth.com/download/dxx/content/descent-pc-shareware.zip
-unzip descent-pc-shareware.zip -d $PACKAGE_NAME/opt/d1x-rebirth/
-rm descent-pc-shareware.zip
+# Download Demo Data
+wget https://web.archive.org/web/20221208193117if_/https://www.dxx-rebirth.com/download/dxx/content/descent2-pc-demo.zip
+unzip descent2-pc-demo.zip -d $PACKAGE_NAME/opt/d2x-rebirth/
+rm descent2-pc-demo.zip
 
-# Hires and Music Packs
-wget https://web.archive.org/web/20230702124034if_/https://www.dxx-rebirth.com/download/dxx/res/d1xr-hires.dxa -O $PACKAGE_NAME/opt/d1x-rebirth/d1xr-hires.dxa || error "failed to download hires pack!"
-wget https://web.archive.org/web/20221208193318if_/https://www.dxx-rebirth.com/download/dxx/res/d1xr-sc55-music.dxa -O $PACKAGE_NAME/opt/d1x-rebirth/d1xr-sc55-music.dxa  || error "failed to download ogg soundtrack!"
+# Rename to lower case (important for Linux case-sensitivity)
+cd $PACKAGE_NAME/opt/d2x-rebirth
+for i in $( ls | grep [A-Z] ); do mv -f $i `echo $i | tr 'A-Z' 'a-z'`; done
+cd -
+
+# Music Pack
+wget https://web.archive.org/web/20221208193318if_/https://www.dxx-rebirth.com/download/dxx/res/d2xr-sc55-music.dxa -O $PACKAGE_NAME/opt/d2x-rebirth/d2xr-sc55-music.dxa
 
 # Configuration
-echo -e "ResolutionX=720\nResolutionY=720\nWindowMode=0" > $PACKAGE_NAME/opt/d1x-rebirth/descent.cfg
+echo -e "ResolutionX=720\nResolutionY=720\nWindowMode=0" > $PACKAGE_NAME/opt/d2x-rebirth/descent.cfg
 
 # Make command asociations
 echo '#!/bin/bash
-cd /opt/d1x-rebirth
-./d1x-rebirth -hogdir /opt/d1x-rebirth "$@"' | tee $PACKAGE_NAME/usr/bin/d1x-rebirth >/dev/null
-chmod +x $PACKAGE_NAME/usr/bin/d1x-rebirth
+cd /opt/d2x-rebirth
+./d2x-rebirth -hogdir /opt/d2x-rebirth "$@"' | tee $PACKAGE_NAME/usr/bin/d2x-rebirth >/dev/null
+chmod +x $PACKAGE_NAME/usr/bin/d2x-rebirth
 
 # Make menu launcher
 echo "[Desktop Entry]
-Name=Descent 1
-Comment=DXX-Rebirth source port of Descent
-Exec=/usr/bin/d1x-rebirth
-Icon=/opt/d1x-rebirth/icon.png
+Name=Descent 2
+Comment=DXX-Rebirth source port of Descent 2
+Exec=/usr/bin/d2x-rebirth
+Icon=/opt/d2x-rebirth/icon.png
 Terminal=false
 Type=Application
 Categories=Game;ActionGame;
-StartupNotify=false" | tee $PACKAGE_NAME/usr/share/applications/d1x-rebirth.desktop >/dev/null
+StartupNotify=false" | tee $PACKAGE_NAME/usr/share/applications/d2x-rebirth.desktop >/dev/null
 
 rm -rf dxx-rebirth
 
